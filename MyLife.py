8731aef580179277e1fd6419e64c9e4c22cf24c8
unknown <templanzatintin@gmail.com>
2023-04-27 00:18:02 +0800

    Greeting

--- MyLife.py
+++ MyLife.py
@@ -1,3 +1,9 @@
 # Templanza, Kristine Joy F.
 # BSCPE 1-4
 # Asignnment no. 3 - Multiple Lines of Text Content
+
+# Greeting and border line
+print("")
+print("\033[35m※ \033[0m" * 35)
+print("")
+print("\033[45m ♥ Welcome! ♥ \033[0m".center(78))
