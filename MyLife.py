6592b45f256eafa4f89e486d7609bc7b1e4d1a0b
unknown <templanzatintin@gmail.com>
2023-04-27 15:27:32 +0800

    Users input is working

--- MyLife.py
+++ MyLife.py
@@ -9,3 +9,8 @@ print("")
 print("\033[45m ♥ Welcome! ♥ \033[0m".center(78))
 
 # Open mylife.txt (read)
+with open("mylife.txt", "w") as input_txt_file:
+    while True:
+        # Ask for the users inpot
+        text = input("\nEnter the line you want to extract in txt file: ")
+        input_txt_file.write(text + "\n")
